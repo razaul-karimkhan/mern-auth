410071477cb8a737dcecb48e41eafd6edc633394
razaul-karim <razaulkarim19725@gmail.com>
2025-03-06 01:53:27 +0600

    Add Routes and Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-       <h1 className='text-danger'> Hello world reza</h1>
- 
-    </div>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
